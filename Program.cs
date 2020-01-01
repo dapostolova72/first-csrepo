b7703bf183bd04971e0d76f54152415861b4c5c4
dapostolova72 <59181442+dapostolova72@users.noreply.github.com>
2020-01-01 17:13:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _5.Преобразуване_на_скорост
+namespace skorost_s_metod
 {
     class Program
-    { static float times(float h, float m, float s)
+    { //vremeto v sekundi//
+        static float times(float h, float m, float s)
         { 
             float time = h * 3600 + m * 60 + s;
             return time;
@@ -30,9 +31,9 @@ namespace _5.Преобразуване_на_скорост
             float rkm = r / 1000;
             return rkm;
         }
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Vavedete razstoianieto v metri");
+        {Console.WriteLine("Vavedete razstoianieto v metri");
             float r = float.Parse(Console.ReadLine());
             Console.WriteLine("Vavedete chasovete");
             float h = float.Parse(Console.ReadLine());
@@ -40,14 +41,12 @@ namespace _5.Преобразуване_на_скорост
             float m = float.Parse(Console.ReadLine());
             Console.WriteLine("Vavedete secundi");
             float s = float.Parse(Console.ReadLine());
-
-            
-           Console.WriteLine(r/times(h,m,s));
+            Console.WriteLine(r/times(h,m,s));
             Console.WriteLine(razskm(r) / timeh(h, m, s));
             Console.WriteLine(mili(r)/timeh(h,m,s));
 
 
-           
+
 
         }
     }
