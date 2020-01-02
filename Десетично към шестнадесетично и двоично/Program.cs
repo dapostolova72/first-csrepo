9310f952d5dfb6a152f61fd55a7eb40b1e8fd559
da <d.apostolova@ptgrz.org>
2020-01-02 19:21:43 +0200

    Add folders

---
+++ Десетично към шестнадесетично и двоично/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Десетично_към_шестнадесетично_и_двоично
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vavedete desetichno chislo");
+            int chislo = int.Parse(Console.ReadLine());
+            string chislohex = Convert.ToString(chislo, 16).ToUpper();
+            Console.WriteLine(chislohex);
+            string chislobinary= Convert.ToString(chislo, 2);
+            Console.WriteLine(chislobinary);
+        }
+    }
+}
